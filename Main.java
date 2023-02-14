366e8e4affc9a319d14ba75e23b726c7e1b88323
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-14 17:34:22 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+package problema_diagonal_negativos;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		 
+		Scanner sc = new Scanner(System.in);
+		
+		int n, i, j;
+		
+		System.out.print("Qual a ordem da matriz? ");
+		n = sc.nextInt();
+		
+		int[][] mat = new int[n][n];
+		
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				System.out.print("Elemento [" + i + " , " + j + "]: ");
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("DIAGONAL PRINCIPAL: ");
+		for (i = 0; i < n; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		
+		System.out.println();
+		
+		int contNegativos = 0;
+		
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				if (mat[i][j] < 0) {
+					contNegativos++;
+				}
+			}
+		}
+		
+		System.out.println("QUANTIDADE DE NEGATIVOS = " + contNegativos);
+		
+		sc.close();
+	}
+}
