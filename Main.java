57d34a450427629e040aee2e5b712e125a2f421c
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-14 17:34:59 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package problema_crescente;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num, num2;
+		
+		System.out.println("Digite dois numeros: ");
+		num = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		while (num != num2) {
+			if (num > num2) {
+				System.out.println("DECRESCENTE");
+			}
+			else {
+				System.out.println("CRESCENTE");
+			}
+			
+			System.out.println("Digite outros dois numeros: ");
+			num = sc.nextInt();
+			num2 = sc.nextInt();
+		}
+		
+		sc.close();
+		
+	}
+}
