44d50ee558444373525c4f1e8017b00bee6656a3
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-14 17:30:55 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+package problema_soma_vetores;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner ssc = new Scanner(System.in);
+		
+		int n, i;
+		System.out.print("Quantos numeros voce vai digitar? ");
+		n = ssc.nextInt();
+		
+		double[] vet = new double[n];
+		
+		for (i = 0; i < n; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = ssc.nextDouble();
+		}
+		
+		System.out.println();
+		
+		System.out.print("VALORES = ");
+		for (i = 0; i < n; i++) {
+			System.out.print(String.format("%.1f", vet[i]) + " ");
+		}
+		
+		System.out.println();
+		
+		double soma, media;
+		soma = 0;
+		for (i = 0; i < n; i++) {
+			soma = soma + vet[i];
+		}
+		
+		System.out.println("SOMA = " + String.format("%.2f", soma));
+		
+		media = soma / n;
+		System.out.println("MEDIA = " + String.format("%.2f", media));
+		
+		ssc.close();
+	}
+}
