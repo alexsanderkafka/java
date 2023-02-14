cb687a89df8e1aeaf0f6a5337c2f5cb5f60612ff
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-14 17:32:11 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package problema_soma_impares;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner ssc = new Scanner(System.in);
+		
+		int x, y, troca, soma;
+		
+		System.out.println("Digite dois numeros inteiros: ");
+		x = ssc.nextInt();
+		y = ssc.nextInt();
+		ssc.close();
+		
+		if (x > y){
+			troca = x;
+			x = y;
+			y = troca;
+		}
+		
+		soma = 0;
+		for (x++; x < y; x++) {
+			if (x % 2 != 0) {
+				soma = soma + x;
+			}
+		}
+		
+		System.out.println("SOMA DOS IMPARES = " + soma);
+		
+	}
+}
